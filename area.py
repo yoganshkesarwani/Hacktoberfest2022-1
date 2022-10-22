724655c39bae25c59e3c3f2803519677ce5943de
yoganshkesarwani <68636131+yoganshkesarwani@users.noreply.github.com>
2022-10-22 17:03:50 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,16 @@
+import math
+pi = math.pi
+def circle(radius):
+     return pi * radius**2
+ 
+def cube(side):
+     return side**3
+ 
+def cylinder(radius, height):
+     return 2*pi*radius + 2*pi*height
+ 
+def sphere(radius):
+     return 2*pi*(radius**2)
+ 
+print(circle(10))
+#You can use other functions to calculate the areas of other shapes.
